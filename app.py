23721566104c55313b27d070970f1b8b70a3e33a
Shogo1811 <shogo.n.1118@gmail.com>
2024-12-21 23:00:50 +0900

    CRUD完成

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template #, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from models import db, Employ
 
@@ -12,7 +12,37 @@ migrate = Migrate(app, db)
 @app.route('/')
 def index():
     employees = Employ.query.all()
-    return render_template('employ_list.html', employees=employees)
+    return render_template('employee_list.html', employees=employees)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_employee():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        department = request.form['department']
+        new_employee = Employ(name=name, email=email, department=department)
+        db.session.add(new_employee)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('employee_form.html', employee=None)
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_employee(id):
+    employee = Employ.query.get_or_404(id)
+    if request.method == 'POST':
+        employee.name = request.form['name']
+        employee.email = request.form['email']
+        employee.department = request.form['department']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('employee_form.html', employee=employee)
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_employee(id):
+    employee = Employ.query.get_or_404(id)
+    db.session.delete(employee)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
